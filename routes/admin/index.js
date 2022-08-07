cdcabe4aa701381afd4ec6571918f9e6aad04aec
Kash15if <kash15if@yahoo.com>
2022-08-07 22:29:34 +0530

    added login route for both

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -10,9 +10,58 @@ const sql = require("mssql");
 //importing db-connection query
 const pool = require("../../models/dbCon"); //importing db-pool for query
 
-router.get("/", async (req, res) => {
+router.get("/test", async (req, res) => {
   const out = await pool.query("SELECT [Date] ,[Time] ,[Menu] FROM [Menu]");
   res.send(out.recordset);
 });
 
+//admin login
+router.post("/login", async (req, res) => {
+  let userCreds = req.body.user;
+
+  try {
+    let dbData = await pool
+      .request()
+      .input("user1", sql.Int, userCreds.user)
+      .query(
+        "SELECT [admin] ,[password] ,[name] FROM [admins] where [admin] = @user1"
+      );
+
+    let foundUser = dbData.recordset[0];
+
+    if (foundUser) {
+      let submittedPass = userCreds.password;
+      let storedPass = foundUser.pass;
+
+      // const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+
+      if (submittedPass === storedPass) {
+        let admin = foundUser.admin;
+        let name = foundUser.name;
+        var token = jwt.sign({ admin }, process.env.AUTHTOKEN);
+
+        res.status(200);
+        res.send({
+          auth: true,
+          token: token,
+          admin: admin,
+          name: name,
+        });
+      } else {
+        res.status(404);
+        res.send("Wrong Password, Type correct password and login again");
+      }
+    } else {
+      // let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+      // await bcrypt.compare(req.body.password, fakePass);
+
+      res.status(404);
+      res.send("No Such user exists");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(405);
+    res.send("Internal server error");
+  }
+});
 module.exports = router;
--- routes/client/index.js
+++ routes/client/index.js
@@ -10,4 +10,104 @@ const sql = require("mssql");
 //importing db-connection query
 const pool = require("../../models/dbCon"); //importing db-pool for query
 
+//user login
+router.post("/login", async (req, res) => {
+  let userCreds = req.body.user;
+
+  try {
+    let dbData = await pool
+      .request()
+      .input("user1", sql.Int, userCreds.user)
+      .query(
+        "SELECT [userid] ,[password] ,[name] ,[dept] FROM [Test_DB].[dbo].[UserDetails] where [userid] = @user1"
+      );
+
+    let foundUser = dbData.recordset[0];
+
+    if (foundUser) {
+      let submittedPass = userCreds.password;
+      let storedPass = foundUser.pass;
+
+      // const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+
+      if (submittedPass === storedPass) {
+        let user = foundUser.user;
+        let name = foundUser.name;
+        var token = jwt.sign({ user }, process.env.AUTHTOKEN);
+
+        res.status(200);
+        res.send({
+          auth: true,
+          token: token,
+          user: user,
+          name: name,
+        });
+      } else {
+        res.status(404);
+        res.send("Wrong Password, Type correct password and login again");
+      }
+    } else {
+      // let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+      // await bcrypt.compare(req.body.password, fakePass);
+
+      res.status(404);
+      res.send("No Such user exists");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(405);
+    res.send("Internal server error");
+  }
+});
+
+//user login
+router.post("/login", async (req, res) => {
+  let userCreds = req.body.user;
+
+  try {
+    let dbData = await pool
+      .request()
+      .input("user1", sql.Int, userCreds.user)
+      .query(
+        "SELECT [userid] ,[password] ,[name] ,[dept] FROM [UserDetails] where [userid] = @user1"
+      );
+
+    let foundUser = dbData.recordset[0];
+
+    if (foundUser) {
+      let submittedPass = userCreds.password;
+      let storedPass = foundUser.pass;
+
+      // const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+
+      if (submittedPass === storedPass) {
+        let user = foundUser.user;
+        let name = foundUser.name;
+        var token = jwt.sign({ user }, process.env.AUTHTOKEN);
+
+        res.status(200);
+        res.send({
+          auth: true,
+          token: token,
+          user: user,
+          name: name,
+        });
+      } else {
+        res.status(404);
+        res.send("Wrong Password, Type correct password and login again");
+      }
+    } else {
+      // let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+      // await bcrypt.compare(req.body.password, fakePass);
+
+      res.status(404);
+      res.send("No Such user exists");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(405);
+    res.send("Internal server error");
+  }
+});
+
 module.exports = router;
