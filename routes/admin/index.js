501518d6a1c7920f3db0a438a8a535b1838e096b
Kash15if <kash15if@yahoo.com>
2022-09-18 21:27:09 +0530

    added mail basic  logic, details to be implemented next

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -634,34 +634,42 @@ router.post("/sendmails", async (req, res) => {
     const users = jwt.decode(token);
 
     let userList = req.body;
+    let month = 8;
 
     userList.forEach(async (oneUser) => {
-      const out = await pool.query("exec Admin_Dashboard");
+      const userList = req.body;
 
-      let transporter = nodeMailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: "xxx@xx.com",
-          pass: "xxxx",
-        },
-      });
-      let mailOptions = {
-        from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
-        to: req.body.to, // list of receivers
-        subject: req.body.subject, // Subject line
-        text: req.body.body, // plain text body
-        html: "<b>NodeJS Email Tutorial</b>", // html body
-      };
+      userList.forEach(async (records) => {
+        const out = await pool.query(
+          "exec [SendBills]" + records.id + "," + month
+        );
 
-      await transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log("Message %s sent: %s", info.messageId, info.response);
-        res.render("index");
+        console.log(out.recordset);
       });
+      // let transporter = nodeMailer.createTransport({
+      //   host: "smtp.gmail.com",
+      //   port: 465,
+      //   secure: true,
+      //   auth: {
+      //     user: "xxx@xx.com",
+      //     pass: "xxxx",
+      //   },
+      // });
+      // let mailOptions = {
+      //   from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
+      //   to: req.body.to, // list of receivers
+      //   subject: req.body.subject, // Subject line
+      //   text: req.body.body, // plain text body
+      //   html: "<b>NodeJS Email Tutorial</b>", // html body
+      // };
+
+      // await transporter.sendMail(mailOptions, (error, info) => {
+      //   if (error) {
+      //     return console.log(error);
+      //   }
+      //   console.log("Message %s sent: %s", info.messageId, info.response);
+      //   res.render("index");
+      // });
     });
 
     res.status(200);
